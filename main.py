438441bf1f852d8544ea70c7430f7d919e92802c
G.SethuPrasad <gsethuprasad.gsp@gmail.com>
2023-11-11 17:25:13 -0700

    Partial output code
    Need to fix some value error apart from that, the code is structured

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 
 #from github import Github
 
+import datetime
+import os
+from bs4 import BeautifulSoup
 import requests
 
 owner_name =""
@@ -59,7 +62,80 @@ class GitHUbRepAnalyser:
         repoData_obj.check()
 
     
+    def collect_pull_requests(self, owner, repo_name):
+        #GitHub API to collect pull requests data
+        url = f'https://api.github.com/search/issues?q=is:pr+repo:{owner}/{repo_name}'
+        response = requests.get(url)
+        pull_request_data = response.json().get('items', [])
 
+        #PullRequest objects corresponding repository details
+        for pr in pull_request_data:
+            title = pr.get('title', '')
+            number = pr.get('number', '')
+            body = pr.get('body', '')
+            state = pr.get('state', '')
+            created_at = pr.get('created_at', '')
+            closed_at = pr.get('closed_at', '')
+            user = pr.get('user', {}).get('login', '')
+            
+            # Additional query to get details like commits, additions, deletions, changed_files
+            pr_details = self.get_pull_request_details(owner, repo_name, number)
+
+            # creating obj for PullRequest class  
+            pull_request = PullRequest(title, number, body, state, created_at, closed_at, user, **pr_details)
+            repo = next((r for r in self.repositories if r.owner == owner and r.name == repo_name), None)
+            if repo:
+                repo.pull_requests.append(pull_request)
+
+    def get_pull_request_details(self, owner, repo_name, number):
+        # query to get details like commits, additions, deletions, changed_files
+        url = f'https://api.github.com/repos/{owner}/{repo_name}/pulls/{number}'
+        response = requests.get(url)
+        pr_details = response.json()
+
+        commits = pr_details.get('commits', 0)
+        additions = pr_details.get('additions', 0)
+        deletions = pr_details.get('deletions', 0)
+        changed_files = pr_details.get('changed_files', 0)
+
+        return {'commits': commits, 'additions': additions, 'deletions': deletions, 'changed_files': changed_files}
+    
+    def collect_user_data(self, username):
+        # Use GitHub API to collect user data
+        url = f'https://api.github.com/users/{username}'
+        response = requests.get(url)
+        user_data = response.json()
+
+        # Extracting relevant information
+        repositories = user_data.get('public_repos', 0)
+        followers = user_data.get('followers', 0)
+        following = user_data.get('following', 0)
+        contributions = self.scrape_user_contributions(username)
+
+        # Creating object for user class
+        user = User(username, repositories, followers, following, contributions)
+        self.save_as_csv('users.csv', user)
+
+    def scrape_user_contributions(self, username):
+        # To Scrape the user contributions from the GitHub profile page as per requirement
+        url = f'https://github.com/{username}?tab=overview&from={datetime.datetime.now().year - 1}-12-01'
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        #contributions_tag = soup.find('h2', class_='f4 text-normal mb-2')
+        contributions = soup.find('h2', class_='f4 text-normal mb-2').text.strip().split()[0]
+        #contributions = contributions_tag.text.strip().split()[0] if contributions_tag else '0'
+        return int(contributions.replace(',', ''))
+    
+    def save_as_csv(self, filename, obj):
+        # Check if the file exists
+        if not os.path.isfile(filename):
+            # Create the file with a header
+            with open(filename, 'w') as file:
+                file.write(obj.get_csv_header() + '\n')
+
+        # Append a new line with the object in CSV format
+        with open(filename, 'a') as file:
+            file.write(obj.to_csv() + '\n')
 
 #repo = GitHUbRepAnalyser('google/jax')
 #details = repo.get_details()
@@ -87,6 +163,37 @@ class Repository:
         print("values in repository class")
         print(f"{self.owner},{self.name},{self.description},{self.homepage},{self.license},{self.forks},{self.watchers}")
 
+
+class PullRequest:
+    def __init__(self, title, number, body, state, created_at, closed_at, user, commits, additions, deletions, changed_files):
+        self.title = title
+        self.number = number
+        self.body = body
+        self.state = state
+        self.created_at = created_at
+        self.closed_at = closed_at
+        self.user = user
+        self.commits = commits
+        self.additions = additions
+        self.deletions = deletions
+        self.changed_files = changed_files
+
+class User:
+    def __init__(self, username, repositories, followers, following, contributions):
+        self.username = username
+        self.repositories = repositories
+        self.followers = followers
+        self.following = following
+        self.contributions = contributions
+
+    def get_csv_header(self):
+        return "username,repositories,followers,following,contributions"
+
+    def to_csv(self):
+        return f"{self.username},{self.repositories},{self.followers},{self.following},{self.contributions}"
+
+
+
 def main():
    print(" Choose one of the bleow options to start exploring & analysing the Repository")
    print("1. If you would like to specify the names of Owner & the repository")
@@ -100,15 +207,22 @@ def main():
       owner_name = input("Please Enter the Owner name : ")
       repositary_name = input("Please Enter the Repositary name : ")
    elif choice == 2:
-      repositoryValue = input("Please, Specify the repository path in 'owner/repository' format  ")
+      repositoryValue = input("Please, Specify the repository path in 'owner/repository' format : ")
       values = repositoryValue.split('/')
       owner_name = values[0]
       repositary_name = values[1]
 
    repositoryAnalyser_obj = GitHUbRepAnalyser()
    repositoryAnalyser_obj.collect_repository_data(owner_name, repositary_name)
-   #repositoryAnalyser_obj.collect_pull_requests(owner_name, repositary_name)
+   repositoryAnalyser_obj.collect_pull_requests(owner_name, repositary_name)
+   
+   #Code to display the list of uses that are gathered from Full requests
 
+
+
+   #To the user name as input to collect the details
+   UserName_to_collectDetails = input("Enter the User name from the above list that you are interested in collect the details :  ")
+   repositoryAnalyser_obj.collect_user_data(UserName_to_collectDetails)
 
 
 main()
